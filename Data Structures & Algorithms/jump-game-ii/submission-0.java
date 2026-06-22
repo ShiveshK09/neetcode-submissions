09caf344d450fd2abf4088cdadfe5e02604a67e4
Shivesh <122783487+ShiveshK09@users.noreply.github.com>
2026-06-22 15:07:25 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump=0;
+        int max=0;
+        int cur=0;
+        for(int i=0;i<nums.length-1;i++){
+            max=Math.max(max,i+nums[i]);
+            if(i==cur){
+                jump++;
+                cur=max;
+            }
+        }
+        return jump;
+    }
+}
